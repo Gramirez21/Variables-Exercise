1930d9dff28189fcfcab2de993db5798fe706cf8
Gramirez21 <r.dannysixty@gmail.com>
2024-04-06 17:44:16 -0700

    Finished

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string trueCoders = "Truecoders";
+            int i = 100;
+            bool Coders = true;
+            double x = 2.2;
+            decimal y = 3.5m;
+            char MiddleInitial = 'A';
+
+            Console.WriteLine($"Learing to code at {trueCoders} tryin to get {i} on all my projects {Coders} i study for {x} hours a day minimum if not {y} to make sure i graduate with an {MiddleInitial}");
         }
     }
 }
